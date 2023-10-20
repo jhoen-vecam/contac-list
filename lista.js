aa234fe1ca1ad9bcde2235d7b1d98a7fb055838d
jhonathan vergara <jhoenjv1996@gmail.com>
2023-10-20 00:29:47 -0500

    "aqui se modifico las funciones y la constante contactos"

--- lista.js
+++ lista.js
@@ -1,24 +1,63 @@
 let contactos = ["jhonathan vergara", "jhoen vecam","israel vergara"];
+ contactos=[{
+  
+  id:1,
+  nombres:"jhonathan ",
+  apellidos:"vergara",
+  teléfono: "07675656",
+  ubicaciones:"barrio el pozon",
+  ciudad:"cartagena",
+  dirección:"pozon",
 
+ },{
+  
+  id:2,
+  nombres:"jhoen ",
+  apellidos:"vecam",
+  teléfono: "0767455",
+  ubicaciones:"barrio el pozon",
+  ciudad:"cartagena",
+  dirección:"pozon",
+
+ },
+ {
+  
+  id:3,
+  nombres:"israel",
+  apellidos:"vergara",
+  teléfono: "07",
+  ubicaciones:"barrio el pozon",
+  ciudad:"cartagena",
+  dirección:"pozon",
+
+ }]
+ //console.log(contactos[1]);
 
 function añadirNuevoContacto(nuevoContacto){
  
     contactos.push(nuevoContacto);
     
 }
-añadirNuevoContacto("rafael orozco" );
-añadirNuevoContacto("poncho zuleta");
+añadirNuevoContacto( 
+  {id:4,
+  nombres:"rafael ",
+  apellidos:"orozco",
+  teléfono: "0767332455",
+  ubicaciones:"barrio el pozon",
+  ciudad:"cartagena",
+  dirección:"pozon",});
 
-console.log("lista de contactos actualizada:");
-console.log( contactos);
+
+console.log("lista de contactos actualizada:" , contactos);
+//console.log( contactos);
 
 function quitarContacto(){
 
-    contactos.pop(contactos)
+    contactos.splice(0,1);
 }
 quitarContacto();
-console.log("lista de contacto actualizada");
-console.log(contactos);
+console.log("lista de contacto actualizada" , contactos);
+//console.log(contactos);
 
 function imprimirContactos() {
   console.log("Lista de contactos:");
@@ -27,15 +66,15 @@ function imprimirContactos() {
   });
  }
   
-  imprimirContactos();
+ // imprimirContactos();
 
-  function añadirNuevoContacto(nuevoContacto){
+ function añadirNuevoContacto(nuevoContacto){
  
-    contactos.push(nuevoContacto);
-    
+   contactos.push(nuevoContacto);
+   
 }
-añadirNuevoContacto("falcao gaarcia" );
+//añadirNuevoContacto("falcao gaarcia" );
 
 
-console.log("lista de contactos actualizada:");
-console.log( contactos);
+
+//console.log( contactos.id[1]);
