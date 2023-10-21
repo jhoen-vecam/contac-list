3299f1c24e8badc223cfdfd7e8dfb5ab41783331
jhonathan vergara <jhoenjv1996@gmail.com>
2023-10-21 00:42:27 -0500

    aqui se actualizo un contacto

--- lista.js
+++ lista.js
@@ -1,80 +1,77 @@
-let contactos = ["jhonathan vergara", "jhoen vecam","israel vergara"];
- contactos=[{
-  
-  id:1,
-  nombres:"jhonathan ",
-  apellidos:"vergara",
-  teléfono: "07675656",
-  ubicaciones:"barrio el pozon",
-  ciudad:"cartagena",
-  dirección:"pozon",
+let contactos = [
+  {
+    id: 1,
+    nombres: "jhonathan ",
+    apellidos: "vergara",
+    teléfono: "07675656",
+    ubicaciones: "barrio el pozon",
+    ciudad: "cartagena",
+    dirección: "pozon",
+  },
+  {
+    id: 2,
+    nombres: "jhoen ",
+    apellidos: "vecam",
+    teléfono: "0767455",
+    ubicaciones: "barrio el pozon",
+    ciudad: "cartagena",
+    dirección: "pozon",
+  },
+  {
+    id: 3,
+    nombres: "israel",
+    apellidos: "vergara",
+    teléfono: "07",
+    ubicaciones: "barrio el pozon",
+    ciudad: "cartagena",
+    dirección: "pozon",
+  },
+];
 
- },{
-  
-  id:2,
-  nombres:"jhoen ",
-  apellidos:"vecam",
-  teléfono: "0767455",
-  ubicaciones:"barrio el pozon",
-  ciudad:"cartagena",
-  dirección:"pozon",
-
- },
- {
-  
-  id:3,
-  nombres:"israel",
-  apellidos:"vergara",
-  teléfono: "07",
-  ubicaciones:"barrio el pozon",
-  ciudad:"cartagena",
-  dirección:"pozon",
-
- }]
- //console.log(contactos[1]);
-
-function añadirNuevoContacto(nuevoContacto){
- 
-    contactos.push(nuevoContacto);
-    
+function añadirNuevoContacto(nuevoContacto) {
+  contactos.push(nuevoContacto);
 }
-añadirNuevoContacto( 
-  {id:4,
-  nombres:"rafael ",
-  apellidos:"orozco",
+
+añadirNuevoContacto({
+  id: 4,
+  nombres: "rafael ",
+  apellidos: "orozco",
   teléfono: "0767332455",
-  ubicaciones:"barrio el pozon",
-  ciudad:"cartagena",
-  dirección:"pozon",});
+  ubicaciones: "barrio el pozon",
+  ciudad: "cartagena",
+  dirección: "pozon",
+});
 
+console.log("Lista de contactos actualizada al agregar un nuevo contacto:", contactos);
 
-console.log("lista de contactos actualizada:" , contactos);
-//console.log( contactos);
-
-function quitarContacto(){
-
-    contactos.splice(0,1);
+function quitarContacto() {
+  contactos.splice(0, 1);
 }
+
 quitarContacto();
-console.log("lista de contacto actualizada:" , contactos);
-//console.log(contactos);
+console.log("Lista de contactos actualizada al quitar un contacto:", contactos);
 
 function imprimirContactos() {
-  console.log("Lista de contactos:");
-  contactos.forEach(function(contacto) {
+  console.log("Lista de contactos impresos:");
+  contactos.forEach(function (contacto) {
     console.log(contacto);
   });
- }
-  
- // imprimirContactos();
-
- function añadirNuevoContacto(nuevoContacto){
- 
-   contactos.push(nuevoContacto);
-   
 }
 
+imprimirContactos();
 
+function actualizarUnContacto() {
+   contactos[1] = {
+    id: 5,
+    nombres: "jhoencitho ",
+    apellidos: "vecam",
+    teléfono: "121212",
+    ubicaciones: "colombia",
+    ciudad: "cartagena",
+    dirección: "pozon",
+  };
+  
+}
 
-
-//console.log( contactos.id[1]);
+actualizarUnContacto();
+console.log("Lista de contactos actualizada después de actualizar un elemento", contactos);
